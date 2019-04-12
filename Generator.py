cdb5995ab8ac26897e3c5e0b72d3b0a0c5c3888b
110283 <leungshuang@aliyun.com>
2019-04-12 15:24:23 +0800

    generator + 杨辉三角

--- Generator.py
+++ Generator.py
@@ -133,4 +133,8 @@ def test_triangles(line_num):
             break
 
 
-test_triangles(1)
+test_triangles(5)
+
+
+# 总结 执行到 yield b 时，fab 函数就返回一个迭代值，
+# 下次迭代时，代码从 yield b 的下一条语句继续执行，而函数的本地变量看起来和上次中断执行前是完全一样的，于是函数继续执行，直到再次遇到 yield。

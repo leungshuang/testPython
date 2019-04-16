72761a770857fe52c1fae9ffd7502196ea6ab733
110283 <leungshuang@aliyun.com>
2019-04-16 13:51:46 +0800

    高阶函数 map reduce应用 lambda 练习

--- AdvancedFunction.py
+++ AdvancedFunction.py
@@ -80,3 +80,56 @@ def str2int_2(s):
 
 
 print(str2int_2('122'))
+
+
+# 作业题：利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，
+# 其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+# 自写
+
+
+def normalize(name):
+    return name.capitalize()
+
+
+def first_upper(lst):
+    return map(normalize, lst)
+
+
+print(list(first_upper(['adam', 'LISA', 'barT'])))
+
+
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+# 自写
+
+
+def prod(lst):
+    return reduce(lambda x, y: x * y, lst)
+
+
+print(prod([3, 5, 7, 9]))
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+
+
+def digits_map(s):
+    digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    return digits[s]
+
+
+def sp_lst(in_str):
+    return in_str.split('.')
+
+
+def int2str(int_in):
+    return reduce(lambda x, y: x * 10 + y, map(digits_map, int_in))
+
+
+def str2float(s):
+    lst = sp_lst(s)
+    int_in = int2str(lst[0])
+    x_in = int2str(lst[1]) / (10 ** len(lst[1]))
+    return int_in + x_in
+
+
+print(str2float('123.456'))
